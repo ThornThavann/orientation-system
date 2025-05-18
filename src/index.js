3800a368bcd15e19be99e565db507822dfc803e4
unknown <thavann.thorn@institute.pse.ngo>
2025-05-18 11:08:39 -0700

    dashbord page

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Overview from './pages/Overview/overview.jsx'; // Make sure the path and filename are correct
 // import Question from './pages/Question/question.jsx';
-
+import DashboardPage from './pages/Dashboard/dashboard.jsx'; // Make sure the path and filename are correct
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Overview />} />  
+      <Route path="/" element={<DashboardPage />} />  
       {/* <Route path="/" element={<Question />} />   */}
 
       
