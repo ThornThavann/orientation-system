a34e09ba450158daa29438e81dd60af402831e27
ThornThavann <thavann.thorn@institute.pse.ngo>
2025-05-19 10:43:44 +0700

    Pull for update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import Overview from './pages/Overview/overview.jsx'; // Make sure the path and filename are correct
-// import Question from './pages/Question/question.jsx';
-
+import Overview from './pages/Overview/overview.jsx';
+import View from './pages/Overview/viewOverview.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Overview />} />  
       {/* <Route path="/" element={<Question />} />   */}
-
       
+       <Route path="/view" element={<View />} />  
+
     </Routes>
   </BrowserRouter>
 );
