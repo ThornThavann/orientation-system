9b3ad7c705718242263dca21e69512a374bb687e
ThornThavann <thavann.thorn@institute.pse.ngo>
2025-05-28 09:44:19 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
+import UpdateQuestion from "./pages/Question/updatequestion.jsx";
+import CreateQuestion from "./pages/Question/createquestion.jsx";
+import ViewSkill from "./pages/Skill/viewskill.jsx";
+import UpdateSkill from "./pages/Skill/updateskill.jsx";
+import CreateSkill from "./pages/Skill/createskill.jsx";
+import Login from "./pages/Auth/login.jsx";
 import Overview from "./pages/Overview/overview.jsx";
 import Question from "./pages/Question/question.jsx";
 import Skill from "./pages/Skill/skill.jsx";
@@ -10,12 +16,7 @@ import CreateMember from "./pages/Member/createmember.jsx";
 import ViewMember from "./pages/Member/viewmember.jsx";
 import DashboardPage from "./pages/Dashboard/dashboard.jsx";
 import ViewQuestion from "./pages/Question/viewquestio.jsx";
-import UpdateQuestion from "./pages/Question/updatequestion.jsx";
-import CreateQuestion from "./pages/Question/createquestion.jsx";
-import ViewSkill from "./pages/Skill/viewskill.jsx";
-import UpdateSkill from "./pages/Skill/updateskill.jsx";
-import CreateSkill from "./pages/Skill/createskill.jsx";
-import Login from "./pages/Auth/login.jsx";
+
 
 import ProtectedRoute from "./pages/Auth/ProtectedRoute.jsx"; // 👈 Import
 
